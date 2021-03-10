e2943056b99157d710cdafffe4d909fe39444f76
OlgaFedorovich <63749183+OlgaFedorovich@users.noreply.github.com>
2021-03-10 23:11:33 +0300

    feat: get countreis info for country details page

--- client/src/components/CountryPage/CountryPage.js
+++ client/src/components/CountryPage/CountryPage.js
@@ -1,6 +1,34 @@
-import React from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";
+import useHttp from "../../hooks/http.hook";
 
-const CountryPage = () => {
+const CountryPage = () => {    
+    const { id } = useParams();
+
+    const [countryData, setCountryData] = useState({});
+
+    const { loading, request } = useHttp();
+
+    const getCountryData = useCallback(
+        async () => {
+            try {
+                // eslint-disable-next-line no-shadow
+                const data = await request('/api/countries/countrylist', 'GET', null);
+                console.log(data);
+                const countryInfo = data.filter(countries => countries.country === id)[0];
+                setCountryData(countryInfo);
+                console.log(countryInfo);
+                // eslint-disable-next-line no-empty
+            } catch (e) {
+            }
+        },
+        [request]
+    );
+
+    useEffect(() => {
+        getCountryData();
+    }, []);
+
     return (
         <div>
             Hello!
