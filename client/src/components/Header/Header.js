7382896fda9ed09651b682eb7d7e5b04111679cb
Lera Chukovich <chukalera@gmail.com>
2021-03-11 17:11:49 +0100

    feat: add styles to navbar and some adaptive styles

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,46 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Input,
+    CardImg
+} from 'reactstrap';
 import './Header.scss';
 import logo from './../../assets/images/travel.svg';
-import {Link} from 'react-router-dom';
 import SearchForm from './../SearchForm/SearchForm';
 
 const Header = ({language, setLanguage, searchValue, setSearchValue}) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+
     return (
-        <nav className="app-header navbar navbar-expand-lg navbar-dark bg-primary">
-            <a className="navbar-brand app-logo" href="#">
-                <img src={logo} alt="logo"/>
-            </a>
-
-            <button className="navbar-toggler collapsed" type="button" data-toggle="collapse"
-                    data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"> </span>
-            </button>
-
-            <div className="collapse navbar-collapse" id="navbarColor01">
-                <div className="navbar-nav mr-auto">
-                    <div className="nav-item">
-                        <Link className='nav-link' to="/login">
-                            Login
-                        </Link>
-                    </div>
-                </div>
-                <SearchForm
-                    searchValue={searchValue}
-                    setSearchValue={setSearchValue}
-                />
-                <div className="nav-item dropdown">
-                    <div className="form-group language-switcher">
-                        <select onChange={(e) => setLanguage(e.target.value)} className="form-control"
-                                id="exampleSelect1" value={language}>
-                            <option value="ru">RU</option>
-                            <option value="en">EN</option>
-                            <option value="blr">BY</option>
-                        </select>
-                    </div>
-                </div>
-            </div>
-        </nav>
+            <Navbar className="app-header navbar navbar-expand-lg navbar-dark bg-primary" expand="md">
+                <NavbarBrand href="/"> <CardImg className='app-logo' src={logo} alt="logo" /></NavbarBrand>
+                <NavbarToggler onClick={toggle}/>
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="me-auto" navbar>
+                        <NavItem>
+                            <NavLink className='nav-link' href="/login/">Login</NavLink>
+                        </NavItem>
+                        <SearchForm
+                            searchValue={searchValue}
+                            setSearchValue={setSearchValue}
+                        />
+                        <Input type="select" name="select" onChange={(e) => setLanguage(e.target.value)}
+                               className="form-control language-switcher"
+                               id="exampleSelect1" value={language}>
+                            <option value='ru'>RU</option>
+                            <option value='en'>EN</option>
+                            <option value='blr'>BY</option>
+                        </Input>
+                    </Nav>
+                </Collapse>
+            </Navbar>
     )
 }
 
