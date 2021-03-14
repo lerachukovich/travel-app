57d564617d0ca5bf5d0bf5868bd3c1b251bf8413
Lera Chukovich <chukalera@gmail.com>
2021-03-14 13:55:59 +0100

    feat: add weather widget

--- client/src/components/CountryPage/CountryPage.js
+++ client/src/components/CountryPage/CountryPage.js
@@ -1,6 +1,8 @@
 import React, { useCallback, useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
 import useHttp from "../../hooks/http.hook";
+import WeatherWidget from "../WetherWidget/WeatherWidget";
+
 
 const CountryPage = () => {
     const { id } = useParams();
@@ -12,44 +14,23 @@ const CountryPage = () => {
     const getCountryData = useCallback(
         async () => {
             try {
-                // eslint-disable-next-line no-shadow
                 const data = await request('/api/countries/countrylist', 'GET', null);
-                console.log(data);
                 const countryInfo = data.filter(countries => countries.country === id)[0];
                 setCountryData(countryInfo);
-                console.log(countryInfo);
-                // eslint-disable-next-line no-empty
             } catch (e) {
             }
         },
         [request]
     );
 
-    const getWeatherData = useCallback(
-        async () => {
-            try {
-                const weatherURL = `http://api.openweathermap.org/data/2.5/weather?q=${countryData.country}&APPID=03c58e90c746d993b00ae6c40ccb8326`;
-                fetch(weatherURL)
-                    .then(res => res.json())
-                    .then(data => {
-                        console.log(data.name)
-                    })
-            } catch (e) {
-            }
-        },
-        [request]
-    )
-
-
 
     useEffect(() => {
         getCountryData();
-        getWeatherData();
     }, []);
 
     return (
         <div>
-            <h1>hello</h1>
+            <WeatherWidget />
         </div>
     )
 };
