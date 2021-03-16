4cd4ddffd7d2ef085359d549e20c5da7a63047cc
Oleg Mikhaylov <dkfolkin@gmail.com>
2021-03-16 17:59:50 +0300

    feat: created border for current country

--- client/src/components/Spinner/Spinner.js
+++ client/src/components/Spinner/Spinner.js
@@ -7,8 +7,8 @@ const Spinner = () => {
         <div className='spinner-wrapper'>
             <svg className='spinner' version="1.1"
                  viewBox="25 25 50 50">
-                <circle cx="50" cy="50" r="20" fill="none" stroke-width="5" stroke="#45d6b5" stroke-linecap="round"
-                        stroke-dashoffset="0" stroke-dasharray="100, 200">
+                <circle cx="50" cy="50" r="20" fill="none" strokeWidth="5" stroke="#45d6b5" strokeLinecap="round"
+                        strokeDashoffset="0" strokeDasharray="100, 200">
                     <animateTransform attributeName="transform" attributeType="XML" type="rotate" from="0 50 50"
                                       to="360 50 50" dur="2.5s" repeatCount="indefinite"/>
                     <animate attributeName="stroke-dashoffset" values="0;-30;-124" dur="1.25s"
