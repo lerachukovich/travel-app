17bc6f05243628368c6f9a66fa79e7de4c9f33b9
Alexus-bat <alexey.bond97@gmail.com>
2021-03-16 14:21:56 +0300

    fix: update

--- routes/api/votes.js
+++ routes/api/votes.js
@@ -6,27 +6,49 @@ const Country = require("../../models/Country");
 // api/votes/rating
 
 router.post("/rating", async (req, res) => {
-    const userId = req.body.userId;
-    const votes = req.body.votes;
-    const countryName = req.body.country;
-    const sight_name = req.body.sight_name;
+    const {userId, votes, country, sight_name} = req.body;
 
     try {
-        // Country.findOne({country_en: country}).then(country => {
-        //     country.sights.map(sight => {
-        //         if (sight.sight_name === sight_name) {
-        //             sight.averageRating.push(votes);
-        //             sight.ratedBy.push(userId);
-        //         }
+        // const updateCountry = await Country.findOne({country_en: country});
 
-        //         return sight
-        //     })
-
-        //     country.save()
+        // updateCountry.sights.map(sight => {
+        //     if (sight.sight_name === sight_name) {
+        //         sight.averageRating.push(votes);
+        //         sight.ratedBy.push(userId);
+        //     }
+        //     return sight
         // })
+        // console.log(updateCountry)
+        // await updateCountry.save();
+        // res.json(updateCountry)
 
+
+
+        
+        const sight = await Country.findOne({country_en: country}).then(data => data.sights);
+
+        sight.map(item => {
+            if (item.sight_name === sight_name) {
+                item.ratedBy.push(userId);
+                item.averageRating.push(votes);
+            }
+            return item;
+        })
+        const changeCountry = await Country.updateOne({country_en: country}, {$set: {sights: sight}});
+        console.log(changeCountry)
+        res.json(sight)
+
+
+
+
+
+
+
+
+
+        
         // Country.findOneAndUpdate({
-        //     country_en: country,
+            //     country_en: country,
         //     sight: sight_name
         // }, {
         //     $push: {
